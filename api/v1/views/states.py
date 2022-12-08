5bbf394418af0373650cb0bf3705c8fc88123860
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 15:04:32 +0100

    State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,6 +7,7 @@ states view for api
 from flask import request, jsonify , abort
 from models import storage
 from models.state import State
+from api.v1.views import app_views
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def list_of_states():
